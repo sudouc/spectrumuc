e35dcfa83745e9d9edc5065309b64a17dfa11f40
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2016-11-20 18:20:07 +1100

    Added Angular2 Router

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By }              from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -9,6 +11,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ RouterTestingModule ]
     });
   });
 
@@ -18,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Spectrum UC'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Spectrum UC');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a the navbar', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    let compiled = fixture.debugElement;
+    expect(compiled.queryAll(By.css('.navbar-brand'))[1].nativeElement.textContent).toContain('Spectrum UC');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
