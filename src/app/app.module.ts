e43255d22cdb17309ce67fe4ee6c11f548ebd046
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2016-11-21 11:52:52 +1100

    Added AngularFire imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
 import { appRoutes } from './app.routes';
+import { StatusPageComponent } from './pages/status-page/status-page.component';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCG2-TArRw69Pkk4Pot1LY5ThFd6f0axI4',
+    authDomain: 'sudo-spectrumuc.firebaseapp.com',
+    databaseURL: 'https://sudo-spectrumuc.firebaseio.com',
+    storageBucket: 'sudo-spectrumuc.appspot.com',
+    messagingSenderId: "521515407409"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
